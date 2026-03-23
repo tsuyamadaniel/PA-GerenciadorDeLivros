b37046c2cc3eae4635c6ee1883d0348484c4f019
Daniel <danieltsuyama1@gmail.com>
2026-03-22 21:31:48 -0300

    criando estrutura do projeto

---
+++ crudlivro/src/main/java/com/exerciciospa/crudlivro/CrudlivroApplication.java
@@ -0,0 +1,13 @@
+package com.exerciciospa.crudlivro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudlivroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudlivroApplication.class, args);
+	}
+
+}
---
+++ crudlivro/src/test/java/com/exerciciospa/crudlivro/CrudlivroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exerciciospa.crudlivro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudlivroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
